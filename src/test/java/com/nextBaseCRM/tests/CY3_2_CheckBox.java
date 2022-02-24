3778004c45851c392f563e1e5f9afef98c28bd1e
Natalia <berluchy@gmail.com>
2022-02-24 13:36:36 -0600

    User story CY3_2 test Automation Done

---
+++ src/test/java/com/nextBaseCRM/tests/CY3_2_CheckBox.java
@@ -0,0 +1,57 @@
+package com.nextBaseCRM.tests;
+
+import com.nextBaseCRM.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+    public class CY3_2_CheckBox {
+        public WebDriver driver;
+
+        @BeforeMethod
+        public void setupMethod() {
+            driver = WebDriverFactory.getDriver("chrome");
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.navigate().to("https://login.nextbasecrm.com");
+        }
+
+
+        @Test
+        public void checkBoxRememberMe_Test1() {
+
+            WebElement rememberPasswordCheckBox = driver.findElement(By.id("USER_REMEMBER"));
+
+
+            Boolean CheckBoxDispl = rememberPasswordCheckBox.isDisplayed();
+
+            Assert.assertTrue(CheckBoxDispl, "CheckBox are not displaying");
+
+        }
+
+        @Test
+        public void checkBoxRememberMe_Test2() {
+
+//5. Locate Text "Remember me on this computer"
+            WebElement checBoxText = driver.findElement(By.xpath("//label[@class='login-item-checkbox-label']"));
+
+
+            String expectedCheckBoxText = "Remember me on this computer";
+            String actualCheckBoxText = checBoxText.getText();
+            Assert.assertEquals(actualCheckBoxText, expectedCheckBoxText, "CheckBox text verification FAILED!!!");
+
+        }
+
+
+        @AfterMethod
+        public void tearDown() {
+            driver.quit();
+        }
+
+    }
