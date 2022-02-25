7a9c2e88e82875f100b83515a73a195d1b929bcb
busrasalamci <salamcibusra@gmail.com>
2022-02-24 23:28:36 -0600

    Test1

---
+++ src/test/java/com/nextBaseCRM/tests/CY3_BusraUserStory.java
@@ -0,0 +1,12 @@
+package com.nextBaseCRM.tests;
+
+import org.testng.annotations.Test;
+
+public class CY3_BusraUserStory extends TestBase{
+
+    @Test
+    public void messsage(){
+        System.out.println(driver.getCurrentUrl());
+    }
+
+}
