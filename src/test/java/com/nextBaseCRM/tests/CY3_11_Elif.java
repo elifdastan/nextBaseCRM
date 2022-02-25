d312219317c3b0c45622096bbb413972b3e4b313
elifdastan <97212348+elifdastan@users.noreply.github.com>
2022-02-24 21:34:10 -0800

    start project

---
+++ src/test/java/com/nextBaseCRM/tests/CY3_11_Elif.java
@@ -0,0 +1,14 @@
+package com.nextBaseCRM.tests;
+
+import org.testng.annotations.Test;
+
+public class CY3_11_Elif extends TestBase{
+
+    @Test
+    public void Elif() {
+        System.out.println(driver.getCurrentUrl());
+
+        // start project
+
+    }
+}
