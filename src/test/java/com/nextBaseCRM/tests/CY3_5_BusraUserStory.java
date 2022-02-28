0c9a528714e1ca84a80bd913fd9b5139e00ce08c
busrasalamci <salamcibusra@gmail.com>
2022-02-27 22:43:15 -0600

    Uptaded code

--- src/test/java/com/nextBaseCRM/tests/CY3_5_BusraUserStory.java
+++ src/test/java/com/nextBaseCRM/tests/CY3_5_BusraUserStory.java
@@ -12,22 +12,63 @@ public class CY3_5_BusraUserStory extends TestBase{
     public void  test1(){
         //1. When users click the MASSAGE tab, they should be able to write the message body and send a message successfully to the feed.
 
-        WebElement sigInButton = driver.findElement(By.xpath("//input[@name ='USER_LOGIN']"));
-        sigInButton.sendKeys("hr7@cydeo.com" + Keys.ENTER);
+       WebElement sigInButton = driver.findElement(By.xpath("//input[@name ='USER_LOGIN']"));
+       sigInButton.sendKeys("hr7@cydeo.com" + Keys.ENTER);
 
-        WebElement passwordButton = driver.findElement(By.xpath("//input[@name ='USER_PASSWORD']"));
-        passwordButton.sendKeys("UserUser" + Keys.ENTER);
+       WebElement passwordButton = driver.findElement(By.xpath("//input[@name ='USER_PASSWORD']"));
+        passwordButton.sendKeys("UserUser");
 
         WebElement logIn= driver.findElement(By.xpath("//input[@value='Log In']"));
         logIn.click();
 
         String expectedTitle = "Portal";
-        String actualTitle = logIn.getText();
+        String actualTitle = driver.getTitle();
 
         Assert.assertEquals(actualTitle,expectedTitle);
 
+        WebElement messageBox = driver.findElement(By.xpath("//span[@class='feed-add-post-micro-title']"));
+        messageBox.click();
 
 
+        WebElement iframe = driver.findElement(By.xpath("//iframe[@class='bx-editor-iframe']"));
+        driver.switchTo().frame(iframe);
+        WebElement messageInputBox = driver.findElement(By.xpath("//body[@contenteditable='true']"));
+        messageInputBox.sendKeys("Hello World");
+
+        driver.switchTo().parentFrame();
+        WebElement sendButton = driver.findElement(By.xpath("(//button[.='Send'])[3]"));
+        sendButton.click();
+
+    }
+
+    @Test
+    public void test2(){
+        WebElement sigInButton = driver.findElement(By.xpath("//input[@name ='USER_LOGIN']"));
+        sigInButton.sendKeys("hr7@cydeo.com" + Keys.ENTER);
+
+        WebElement passwordButton = driver.findElement(By.xpath("//input[@name ='USER_PASSWORD']"));
+        passwordButton.sendKeys("UserUser");
+
+        WebElement logIn= driver.findElement(By.xpath("//input[@value='Log In']"));
+        logIn.click();
+
+        String expectedTitle = "Portal";
+        String actualTitle = driver.getTitle();
+
+        Assert.assertEquals(actualTitle,expectedTitle);
+
+        WebElement messageBox = driver.findElement(By.xpath("//span[@class='feed-add-post-micro-title']"));
+        messageBox.click();
+
+        WebElement sendButton = driver.findElement(By.xpath("(//button[.='Send'])[3]"));
+        sendButton.click();
+
+        WebElement errorMessage = driver.findElement(By.xpath("//span[.='The message title is not specified']"));
+
+        String expectedText = "The message title is not specified";
+        String actualText = errorMessage.getText();
+
+        Assert.assertEquals(actualText,expectedText);
     }
 
 }
